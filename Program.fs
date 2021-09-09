5f8331fc9db3a9ddedb5cba5bef99e8d61abac21
Michael Rieder <1219037+miried@users.noreply.github.com>
2021-09-09 15:41:00 +0200

    F# hello world

---
+++ Program.fs
@@ -0,0 +1,8 @@
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello world %i" 1
+    0 // return an integer exit code
